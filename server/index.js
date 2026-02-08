b9214ec149bd63a540feac648e2a23bd08450cc8
teburke111 <teburke111@gmail.com>
2026-02-08 14:34:16 -0500

    add server-side

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`Server running.`);
+});
